9c5c964e63b2e5977e10f3d2fe11a467b4e0f61e
nicklllson <nikita.vasia2015@gmail.com>
2024-02-29 13:55:40 +0500

    Сделал тестовую связь с бэкендом

--- src/panels/catalog/Catalog.tsx
+++ src/panels/catalog/Catalog.tsx
@@ -7,12 +7,26 @@ import {
 import Search from '../../components/search/Search';
 import Genres from '../../components/genres/Genres';
 import Relevants from '../../components/relevant/Relevants';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 type Props = {
 	id: string
 }
 
 export default function Catalog({ id }: Props) {
+
+	const [author, setAuthor] = useState([]);
+
+	useEffect(() => {
+		const createAuthor = async () => {
+			const { data } = await axios.post('http://localhost:3100/api/author/create', { name: 'Nick' })
+			console.log(data);
+		}
+
+		createAuthor();
+	}, [])
+
 	return (
 		<Panel id={id}>
 
