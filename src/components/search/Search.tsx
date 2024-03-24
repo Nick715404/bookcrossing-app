f2bb26761096bf963a4e39e8354ac61419b7cecc
freakyAion <FreakyAion@yandex.ru>
2024-03-25 02:09:54 +0500

    I screwed up with branches. :(

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -4,12 +4,27 @@ import { $search, searchChanged } from "../../store/search";
 import { Search as SearchPanel } from "@vkontakte/vkui"
 import { useCallback } from "react";
 
+import { $searchResult } from "../../store/search";
+import { $books } from "../../store/books";
+
 function Search() {
 
-  const [search, handleSearch] = useUnit([$search, searchChanged]);
+  const [search, handleSearch, books] = useUnit([$search, searchChanged, $books]);
+
+  const [searchResult] = useUnit($searchResult);
+
+  
 
   const onSearchChange = useCallback((event) => {
     handleSearch(event.target.value)
+    console.clear();
+    console.log(searchResult);
+    console.log(books);
+    console.log($books);
+    const filteredBooks = books.filter((book) =>
+      book.title.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    console.log(filteredBooks);
   }, [handleSearch]);
 
   return (
