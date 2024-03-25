1ae7562cfb571adbb1e7c0eadded3e58e82604d9
nicklllson <nikita.vasia2015@gmail.com>
2024-03-25 17:36:36 +0500

    New update!

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,12 @@ import '@vkontakte/vkui/dist/vkui.css';
 import { RouterProvider } from "@vkontakte/vk-mini-apps-router";
 import { appRouter } from "./constants/vk-router";
 import UserProvider from "./utilities/UserProvider";
-import bridge from "@vkontakte/vk-bridge";
-import { AdaptivityProvider, ConfigProvider, AppRoot } from "@vkontakte/vkui";
-import ReactDOM, { render } from "react-dom";
 import InitAppEntities from "./init-app-entities/InitAppEntities";
 import InitAppModals from "./init-app-entities/InitAppModals";
 import Router from "./router/Router";
+import bridge from "@vkontakte/vk-bridge";
+import ReactDOM, { render } from "react-dom";
+import { AdaptivityProvider, ConfigProvider, AppRoot } from "@vkontakte/vkui";
 
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
