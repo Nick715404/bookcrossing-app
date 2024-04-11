ee3e81e8da5db12cfb81ff2d805e93812d7e4502
MMAksimm123 <maksim-melnik-mel@mail.ru>
2024-04-11 21:27:08 +0500

    Дополнил словарь недостающими картинками категорий

--- src/dictionary/dictionary.tsx
+++ src/dictionary/dictionary.tsx
@@ -9,15 +9,21 @@ import
   Icon24HieroglyphCharacterOutline, 
   Icon24HorseToyOutline, 
   Icon24PaletteOutline, 
+  Icon24RectangleSplit4UnevenOutline, 
+  Icon24StarsOutline, 
   Icon24StatisticsOutline, 
+  Icon24Users3Outline, 
+  Icon28EducationOutline, 
   Icon28SchoolOutline,
   Icon28Smiles2Outline} from "@vkontakte/icons"
 
 export const iconMap: any = {
     Icon24BookSpreadOutline: <Icon24BookSpreadOutline style={{color: '#2688EB'}} />,
+    Icon24RectangleSplit4UnevenOutline: <Icon24RectangleSplit4UnevenOutline style={{color: '#2688EB'}} />,
     Icon24HieroglyphCharacterOutline: <Icon24HieroglyphCharacterOutline style={{color: '#2688EB'}} />,
     Icon24HorseToyOutline: <Icon24HorseToyOutline style={{color: '#2688EB'}} />,
     Icon2416CircleOutline: <Icon2416CircleOutline style={{color: '#2688EB'}} />,
+    Icon28EducationOutline: <Icon28EducationOutline style={{color: '#2688EB'}} />,
     Icon24HealthOutline: <Icon24HealthOutline style={{color: '#2688EB'}} />,
     Icon24BeautyOutline: <Icon24BeautyOutline style={{color: '#2688EB'}} />,
     Icon24ChefHatOutline: <Icon24ChefHatOutline style={{color: '#2688EB'}} />,
@@ -26,5 +32,7 @@ export const iconMap: any = {
     Icon28SchoolOutline: <Icon28SchoolOutline style={{color: '#2688EB'}} />,
     Icon28Smiles2Outline: <Icon28Smiles2Outline style={{color: '#2688EB'}} />,
     Icon24PaletteOutline: <Icon24PaletteOutline style={{color: '#2688EB'}} />,
-    Icon24StatisticsOutline: <Icon24StatisticsOutline style={{color: '#2688EB'}} />
+    Icon24StatisticsOutline: <Icon24StatisticsOutline style={{color: '#2688EB'}} />,
+    Icon24StarsOutline: <Icon24StarsOutline style={{color: '#2688EB'}} />,
+    Icon24Users3Outline: <Icon24Users3Outline style={{color: '#2688EB'}} />
 }
