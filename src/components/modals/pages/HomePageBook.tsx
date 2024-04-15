8eeb1ee45ad4053627fd1dd1d95e82e34cd9651c
MMAksimm123 <maksim-melnik-mel@mail.ru>
2024-04-15 14:32:45 +0500

    useEffect

--- src/components/modals/pages/HomePageBook.tsx
+++ src/components/modals/pages/HomePageBook.tsx
@@ -50,24 +50,24 @@ const HomePageBook = ({ id }: Props) => {
         getCurrentBook();
     }, []);
 
-    useEffect(() => {
-        console.log(id)
-        if (!book) {
-            const getCurrentBook = async () => {
-                try {
-                    const data = await getCurentBookFX(paramsId);
-                    setBook(data);
-                    const images = await getBookImage(paramsId);
-                    if (!images) return
-                    setPath(images ? images.path : '');
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-            getCurrentBook();
-            console.log(getBook)
-        }
-    }, [id]);
+    // useEffect(() => {
+    //     console.log(id)
+    //     if (!book) {
+    //         const getCurrentBook = async () => {
+    //             try {
+    //                 const data = await getCurentBookFX(paramsId);
+    //                 setBook(data);
+    //                 const images = await getBookImage(paramsId);
+    //                 if (!images) return
+    //                 setPath(images ? images.path : '');
+    //             } catch (error) {
+    //                 console.log(error);
+    //             }
+    //         }
+    //         getCurrentBook();
+    //         console.log(getBook)
+    //     }
+    // }, [id]);
 
     const headerBefore = (
         <PanelHeaderBack label="Назад" onClick={() => window.history.back()} />
