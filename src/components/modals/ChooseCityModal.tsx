580e13e8ce3d9656b1d3cbe5d9a109328a0d0b1d
nicklllson <nikita.vasia2015@gmail.com>
2024-04-25 21:45:56 +0500

    Fix: Уьрал состояние из ChooseCityModal

--- src/components/modals/ChooseCityModal.tsx
+++ src/components/modals/ChooseCityModal.tsx
@@ -3,18 +3,16 @@ import { useCities } from '../../hooks/useCities';
 import { Div, Group, ModalPage, ModalPageHeader, } from '@vkontakte/vkui';
 import { Loader } from '../../utilities/cities/Loader';
 import { ChooseCity } from '../ChooseCity/ChooseCity';
-import { useState } from 'react';
 
 type Props = {
   id: string
 }
 
 function ChooseCityModal({ id }: Props) {
-  const [isCityChosen, setIsCityChosen] = useState(false);
   const { data, isFetching, isSuccess } = useCities();
 
   const handleClose = () => {
-    if (isCityChosen) setStatusActiveModal(null);
+    setStatusActiveModal(null);
   }
 
   return (
