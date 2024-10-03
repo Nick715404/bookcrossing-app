18aea9f908aa1ee850fe6753718ebc3529ed22b8
Nick715404 <nikita.vasia2015@gmail.com>
2024-10-03 23:03:44 +0500

    test

--- src/api/server/user/user.ts
+++ src/api/server/user/user.ts
@@ -1,48 +1,46 @@
-import { api } from "../../axios/axiosInstance";
-import { IVkUser } from "../../../interfaces/interface";
-import { createEffect } from "effector";
+import { api } from '../../axios/axiosInstance';
+import { IVkUser } from '../../../interfaces/interface';
+import { createEffect } from 'effector';
 
 export const fetchUserFromDataBase = async (id: number) => {
-  try {
-    const response = await api.get(`/user/find/${id}`);
+	try {
+		const response = await api.get(`/user/find/${id}`);
 
-    if (response.statusText !== 'OK') {
-      return {
-        status: 'empty',
-        user: null,
-      }
-    }
+		if (response.statusText !== 'OK') {
+			return {
+				status: 'empty',
+				user: null,
+			};
+		}
 
-    return {
-      status: 'founded',
-      user: response.data,
-    }
-  } catch (error) {
-    return {
-      status: 'empty',
-      user: null,
-    }
-  }
+		return {
+			status: 'founded',
+			user: response.data,
+		};
+	} catch (error) {
+		return {
+			status: 'empty',
+			user: null,
+		};
+	}
 };
 
 export const GetCurrentUserFX = createEffect(async (user: IVkUser) => {
+	if (!user) return new Error('User not found!');
 
-  if (!user) return new Error('User not found!');
-
-  try {
-    const { data } = await api.post('/user/create', user);
-    return data.user;
-  }
-  catch (error) {
-    return new Error('Error with server');
-  }
+	try {
+		const { data } = await api.post('/user/create', user);
+		return data.user;
+	} catch (error) {
+		return new Error('Error with server');
+	}
 });
 
 export const GetCurrentUserFromServerFX = createEffect(async (id: number) => {
-  try {
-    const { data } = await api.get(`/user/find/${id}`);
-    return data;
-  } catch (error) {
-    throw new Error('Error with server');
-  }
+	try {
+		const { data } = await api.get(`/user/find/${id}`);
+		return data;
+	} catch (error) {
+		throw new Error('Error with server');
+	}
 });
--- src/utilities/providers/UserProvider.tsx
+++ src/utilities/providers/UserProvider.tsx
@@ -1,34 +1,37 @@
-import { useActiveVkuiLocation, useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
-import { GetCurrentUserFX, GetCurrentUserFromServerFX, fetchUserFromDataBase } from '../../api/server/user/user';
+import {
+	useActiveVkuiLocation,
+	useRouteNavigator,
+} from '@vkontakte/vk-mini-apps-router';
+import {
+	GetCurrentUserFX,
+	GetCurrentUserFromServerFX,
+	fetchUserFromDataBase,
+} from '../../api/server/user/user';
 import { fetchVkUser } from '../../api/vk-bridge/user';
 import { setStatusActiveModal } from '../../store/activeModal';
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
 type Props = {
-  children: React.ReactNode
-}
+	children: React.ReactNode;
+};
 
 export default function UserProvider({ children }: Props) {
-  const router = useRouteNavigator();
-  useEffect(() => {
-    const fetchData = async () => {
-      const userData = await fetchVkUser();
-      const serverUser = await fetchUserFromDataBase(userData.id);
-      
-      if (serverUser.status === "empty") {
-        router.push('/onboarding')
-        return;
-      }
+	const router = useRouteNavigator();
+	useEffect(() => {
+		const fetchData = async () => {
+			const userData = await fetchVkUser();
+			const serverUser = await fetchUserFromDataBase(userData.id);
 
-      GetCurrentUserFromServerFX(userData.id);
-    };
+			if (serverUser.status === 'empty') {
+				router.push('/onboarding');
+				return;
+			}
 
-    fetchData();
-  }, []);
+			GetCurrentUserFromServerFX(userData.id);
+		};
 
-  return (
-    <>
-      {children}
-    </>
-  )
+		fetchData();
+	}, []);
+
+	return <>{children}</>;
 }
