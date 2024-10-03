ba2cfddaed86c025ad98dcd16faa22a1c9e46a90
Nick715404 <nikita.vasia2015@gmail.com>
2024-10-03 09:27:53 +0500

    minor updates

--- src/components/book/Book.tsx
+++ src/components/book/Book.tsx
@@ -1,55 +1,52 @@
-import { IBook } from "../../interfaces/interface";
-import { selectBookFX } from "../../store/modalBook";
-
-import { useActiveVkuiLocation, useRouteNavigator } from "@vkontakte/vk-mini-apps-router";
 import {
-  Div,
-  SimpleCell,
-  Text,
-  SplitLayout,
-} from "@vkontakte/vkui";
-import { CustomImage } from "../CustomImage/CustomImage";
+	useActiveVkuiLocation,
+	useRouteNavigator,
+} from '@vkontakte/vk-mini-apps-router';
+import { IBook } from '../../interfaces/interface';
+import { selectBookFX } from '../../store/modalBook';
+import { Div, SimpleCell, Text, SplitLayout } from '@vkontakte/vkui';
+import { CustomImage } from '../CustomImage/CustomImage';
 
 type Props = {
-  book: IBook
-  afterIcon: React.ReactNode | null
-  beforeIcon: React.ReactNode | null
-}
+	book: IBook;
+	afterIcon: React.ReactNode | null;
+	beforeIcon: React.ReactNode | null;
+};
 
 export default function Book({ book, afterIcon, beforeIcon }: Props) {
-  const navigator = useRouteNavigator();
-  const { panel: activePanel } = useActiveVkuiLocation();
+	const navigator = useRouteNavigator();
+	const { panel: activePanel } = useActiveVkuiLocation();
 
-  const handleChooseBook = () => {
-    selectBookFX(book)
-    navigator.push(`/book/${book.id}`);
-  }
+	const handleChooseBook = () => {
+		selectBookFX(book);
+		navigator.push(`/book/${book.id}`);
+	};
 
-  return (
-    <SplitLayout>
-      <Div className="book" id={book.id}>
-        <SimpleCell
-          className="book-wrapper"
-          before={<CustomImage bookId={book.id} />}
-          selected={activePanel === 'book-panel'}
-          onClick={handleChooseBook}
-        >
-          <Text className="book-title" weight="1">
-            {book.title}
-          </Text>
-          <Text className="book-author book-info">
-            {book.author ? book.author : 'Автор не найден'}
-          </Text>
-          <Text className="book-quality book-info">
-            {book.state ? book.state : "Не найдено"}
-          </Text>
-          <Text className="book-genre book-info">
-            {book.categoryTitle ? book.categoryTitle : 'Нет жанра'}
-          </Text>
-        </SimpleCell>
-      </Div >
-      <>{afterIcon}</>
-      <>{beforeIcon}</>
-    </SplitLayout >
-  )
+	return (
+		<SplitLayout>
+			<Div className='book' id={book.id}>
+				<SimpleCell
+					className='book-wrapper'
+					before={<CustomImage bookId={book.id} />}
+					selected={activePanel === 'book-panel'}
+					onClick={handleChooseBook}
+				>
+					<Text className='book-title' weight='1'>
+						{book.title}
+					</Text>
+					<Text className='book-author book-info'>
+						{book.author ? book.author : 'Автор не найден'}
+					</Text>
+					<Text className='book-quality book-info'>
+						{book.state ? book.state : 'Не найдено'}
+					</Text>
+					<Text className='book-genre book-info'>
+						{book.categoryTitle ? book.categoryTitle : 'Нет жанра'}
+					</Text>
+				</SimpleCell>
+			</Div>
+			<>{afterIcon}</>
+			<>{beforeIcon}</>
+		</SplitLayout>
+	);
 }
