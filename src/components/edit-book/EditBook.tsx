30553363d8e6ba827711f8b75812f31086ec98f4
Nick715404 <nikita.vasia2015@gmail.com>
2024-10-23 08:51:39 +0500

    finish

--- src/components/edit-book/EditBook.tsx
+++ src/components/edit-book/EditBook.tsx
@@ -1,28 +1,26 @@
 import { useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
 import { vkBlueColor } from '../../constants/utils';
-import { getCurentBookFX } from '../../api/server/books/books';
 
 import { Icon28WriteOutline } from '@vkontakte/icons';
 import { IconButton } from '@vkontakte/vkui';
 
 type Props = {
-  bookId: string
-}
+	bookId: string;
+};
 
 export default function EditBook({ bookId }: Props) {
+	const navigator = useRouteNavigator();
 
-  const navigator = useRouteNavigator()
+	const handleClick = () => {
+		navigator.push(`/${bookId}`);
+		// getCurentBookFX(bookId);
+	};
 
-  const handleClick = () => {
-    navigator.push(`/${bookId}`)
-    getCurentBookFX(bookId);
-  };
-
-  return (
-    <>
-      <IconButton onClick={handleClick} className='edit-book'>
-        <Icon28WriteOutline fill={vkBlueColor} />
-      </IconButton>
-    </>
-  )
-} 
+	return (
+		<>
+			<IconButton onClick={handleClick} className='edit-book'>
+				<Icon28WriteOutline fill={vkBlueColor} />
+			</IconButton>
+		</>
+	);
+}
--- src/hooks/useCurrentBook.tsx
+++ src/hooks/useCurrentBook.tsx
@@ -1,30 +1,29 @@
-import React from 'react'
+import React from 'react';
 import { useQuery, useQueryClient } from 'react-query';
 import { getCurrentBook } from '../api/server/books/books';
 
 type Props = {
-  bookId: string;
-  paramsId: string | undefined;
-}
+	bookId: string;
+	paramsId: string | undefined;
+};
 
 const useCurrentBook = ({ bookId, paramsId }: Props) => {
+	const client = useQueryClient();
 
-  const client = useQueryClient();
-
-  return useQuery({
-    queryKey: ['serverBook'],
-    queryFn: async () => {
-      if (bookId === '') {
-        return await getCurrentBook(paramsId)
-      }
-    },
-    refetchOnWindowFocus: false,
-    onSuccess() {
-      client.invalidateQueries({
-        queryKey: [['owner', 'city']]
-      })
-    }
-  })
-}
+	return useQuery({
+		queryKey: ['serverBook'],
+		queryFn: async () => {
+			if (bookId === '') {
+				return await getCurrentBook(paramsId);
+			}
+		},
+		refetchOnWindowFocus: false,
+		onSuccess() {
+			client.invalidateQueries({
+				queryKey: [['owner', 'city']],
+			});
+		},
+	});
+};
 
 export { useCurrentBook };
